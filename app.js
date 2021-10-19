8bf93bf08fe083b25b085729615759a3b76b0535
shiva <shivarv1993@gmail.com>
2021-10-19 22:49:50 +0100

    commit logic implementation

--- app.js
+++ app.js
@@ -33,20 +33,27 @@ io.sockets.on('connection',
     console.log("We have a new client: " + socket.id);
   
     // When this user emits, client side: socket.emit('otherevent',some data);
-    socket.on('mouse',
+    socket.on('playershoot',
       function(data) {
         // Data comes in as whatever was sent, including objects
-        console.log("Received: 'mouse' " + data.x + " " + data.y);
-      
+        console.log("Received: 'playershootevent' " + data.playerType);
         // Send it to all other clients
-        socket.broadcast.emit('mouse', data);
-        
+        socket.broadcast.emit('playershoot', data);
         // This is a way to send to everyone including sender
         // io.sockets.emit('message', "this goes to everyone");
-
       }
     );
-    
+
+    socket.on('playermoved',
+      function(data) {
+        // Data comes in as whatever was sent, including objects
+        console.log("Received: 'datamovedevent' " + data.playerType + ' '+data.keyCode);
+        socket.broadcast.emit('playermoved', data);
+        // io.sockets.emit('message', "this goes to everyone");
+      }
+    );
+
+
     socket.on('disconnect', function() {
       console.log("Client has disconnected");
     });
--- public/sketch.js
+++ public/sketch.js
@@ -1,75 +1,292 @@
-var socket;
+let gameStarted = false;
+let redCircle;
+let circleRadius = 50;
+let blueCircle;
+
+let isFirst = false;
+let socket;
+
+let vectorRedCircleColor =  new p5.Vector(255, 0, 0);
+let vectorBlueCircleColor = new p5.Vector(0, 0, 255);
+let height = 400;
+let width = 400;
+
+let bulletSize = 10;
+
+let redFireBullets = [];
+let blueFireBullets = [];
+
+let createdRedSquare = [];
+let createdBlueSquare = [];
+
+let input, button, playerIdMessage, selectList, choosePlayerTypeMessage;
+
+let currentPlayerType;
+let playerId;
+
+let elementsToRemove = [];
 function setup() {
-    createCanvas(400, 400);
-    background(51);
-    socket =  io().connect('http://localhost:3000');
-    socket.on('mouse', newDrawing);
+  let elementPositionMap = {};
+  elementPositionMap.choosePlayerTypeMessage =  {x: 20, y: 5};
+  elementPositionMap.selectPlayer = {x: 20, y: 50};
+  elementPositionMap.playerIdMessage = {x: 20, y: 70};
+  elementPositionMap.input =  {x: 20, y: 120};
 
-}
-
-function newDrawing(data) {
-    console.log('in new Drawing recieved ');
-}
-
-function draw() {
-    noStroke();
-    fill(255);
-    ellipse(mouseX, mouseY, 36, 36);
-}
-/*
-// ITP Networked Media, Fall 2014
-// https://github.com/shiffman/itp-networked-media
-// Daniel Shiffman
-
-// Keep track of our socket connection
-var socket;
-
-function setup() {
-  createCanvas(400, 400);
-  background(0);
-  // Start a socket connection to the server
-  // Some day we would run this server somewhere else
-  socket = io.connect('http://localhost:3000');
-  // We make a named event called 'mouse' and write an
-  // anonymous callback function
-  socket.on('mouse',
-    // When we receive data
-    function(data) {
-      console.log("Got: " + data.x + " " + data.y);
-      // Draw a blue circle
-      fill(0,0,255);
-      noStroke();
-      ellipse(data.x, data.y, 20, 20);
-    }
-  );
-}
-
-function draw() {
-  // Nothing
-}
-
-function mouseDragged() {
-  // Draw some white circles
-  fill(255);
-  noStroke();
-  ellipse(mouseX,mouseY,20,20);
-  // Send the mouse coordinates
-  sendmouse(mouseX,mouseY);
-}
-
-// Function for sending to the socket
-function sendmouse(xpos, ypos) {
-  // We are sending!
-  console.log("sendmouse: " + xpos + " " + ypos);
+  createCanvas(height, width);
+  textAlign(CENTER);
   
-  // Make a little object with  and y
-  var data = {
-    x: xpos,
-    y: ypos
-  };
+  choosePlayerTypeMessage = createElement('h3', 'Choose Player Type'); choosePlayerTypeMessage.position
+  (elementPositionMap['choosePlayerTypeMessage'].x,         elementPositionMap['choosePlayerTypeMessage'].y);
+  addElementToRemove(choosePlayerTypeMessage);
+  selectList = createSelect();
+  selectList.option('player1');
+  selectList.option('player2');
+  selectList.selected('player1');
+  currentPlayerType = 'player1';
+  selectList.changed(selectPlayerType);
+  selectList.position(elementPositionMap['selectPlayer'].x, elementPositionMap['selectPlayer'].y);
+  addElementToRemove(selectList);
+  playerIdMessage = createElement('h3', 'Choose Player Id');           playerIdMessage.position
+  (elementPositionMap['playerIdMessage'].x,         elementPositionMap['playerIdMessage'].y);
+  addElementToRemove(playerIdMessage);
+   input = createInput();
+  input.position(elementPositionMap['input'].x, elementPositionMap['input'].y);
+  addElementToRemove(input);
 
-  // Send that object to the socket
-  socket.emit('mouse',data);
+  button = createButton('submit');
+  button.position(input.x + input.width + 10, elementPositionMap['input'].y);
+  addElementToRemove(button);
+  button.mousePressed(gameStart);  
+ 
 }
 
+function addElementToRemove(ele) {
+  elementsToRemove.push(ele);
+}
+
+function selectPlayerType() {
+    console.log('selectPlayerType '+selectList.value());
+  currentPlayerType = selectList.value();
+}
+
+function gameStart() {
+  createCanvas(height, width);
+  gameStarted = true;
+  socket =  io().connect('http://localhost:3000');
+  socket.on('playershoot', playerShootEventHandler);
+  socket.on('playermoved', playerMovedEventHandler)
+  //socket.on('serverevent', newServerEvent);
+  for(let i in elementsToRemove) {
+    elementsToRemove[i].remove();
+  }
+  redCircle = new p5.Vector(width / 2, circleRadius/2);
+  blueCircle = new p5.Vector(width / 2 ,height - circleRadius / 2);
+}
+
+function playerMovedEventHandler(data) {
+    console.log('playerMovedEventHandler subscribed');
+    moveCharacterType(data.playerType, data.keyCode, false);
+}
+
+function playerShootEventHandler(data) {
+    console.log('playershootHandler subscribed');
+    fireBulletBasedOnPlayerType(data.playerType);
+}
+
+function fireBulletBasedOnPlayerType(playerType) {
+    if(playerType === 'player1') {
+        fireBullet('blue');
+    } else if(playerType === 'player2') {
+        fireBullet('red');
+    }
+}
+
+function isGameStarted() {
+    return gameStarted === true;
+}
+
+function draw(){
+  
+  if(!isGameStarted()) {
+    return;
+  }
+  
+
+  createdRedSquare = [];
+  createdBlueSquare = [];
+  /*
+  redCircle.x++;
+  if(redCircle.x > width) {
+    redCircle.x = 0;
+  }
+
+  blueCircle.y++;
+  if(blueCircle.y > height) {
+    blueCircle.y = 0;
+  }
 */
+  background(50);
+
+  fill(vectorRedCircleColor.x, vectorRedCircleColor.y, vectorRedCircleColor.z);
+
+  //ellipse wants parameter of diamater not radius
+  ellipse(redCircle.x, redCircle.y, circleRadius, circleRadius);
+
+  fill(vectorBlueCircleColor.x, vectorBlueCircleColor.y, vectorBlueCircleColor.z);
+   //ellipse wants parameter of diamater not radius
+  ellipse(blueCircle.x, blueCircle.y, circleRadius, circleRadius);
+  fill(vectorRedCircleColor.x, vectorRedCircleColor.y, vectorRedCircleColor.z);
+  for(let i = 0; i < redFireBullets.length; i++) {
+      square(redFireBullets[i].currentLocation.x, redFireBullets[i].currentLocation.y, bulletSize);
+      redFireBullets[i].currentLocation.y += 1;
+    if(redFireBullets[i].currentLocation.y  >= (height - bulletSize * 2)) {
+      console.log('reached the end ');
+       redFireBullets.splice(i, 1);
+    }
+    
+  }
+  
+  fill(vectorBlueCircleColor.x, vectorBlueCircleColor.y, vectorBlueCircleColor.z);
+  for(let i = 0; i < blueFireBullets.length; i++) {
+      square(blueFireBullets[i].currentLocation.x, blueFireBullets[i].currentLocation.y - bulletSize/2, bulletSize);
+      blueFireBullets[i].currentLocation.y -= 1;
+    if(blueFireBullets[i].currentLocation.y <= bulletSize * 2) {
+       blueFireBullets.splice(i, 1);
+    }
+  }
+  
+  checkforIntersection();
+  checkForPlayerHit();
+}
+
+function mouseClicked(event) {
+  if(isGameStarted()) {
+    fireBulletBasedOnPlayerType(currentPlayerType);
+    console.log('currentPlayerType '+currentPlayerType);
+    socket.emit('playershoot', {playerType: currentPlayerType});
+  }
+}
+
+function keyPressed() {
+    console.log('keypressed');
+  if(!isGameStarted()) {
+    return;
+  }
+  try{
+    moveCharacterType(currentPlayerType, keyCode, true);
+
+  } catch(e) {
+      console.log('exception ');
+      console.log(e);
+  }
+}
+
+function moveCharacterType(playerType, keyCodeRecieved, isPublishEvent) {
+    console.log('moveCharacter keycode '+keyCodeRecieved);
+    let circle = getCircleToMove(playerType);
+    if (keyCodeRecieved === LEFT_ARROW) {
+        moveCharacter(circle, true);
+    } else if (keyCodeRecieved === RIGHT_ARROW) {
+        moveCharacter(circle, false);
+    } else if (keyCodeRecieved === ENTER) {
+        fireBullet('blue');
+    }
+    if(isPublishEvent) {
+        socket.emit('playermoved', {playerType: playerType, keyCode: keyCodeRecieved});
+    }
+}
+
+function fireBullet(colorType)
+{
+    if(colorType === 'blue') {
+      blueFireBullets.push({value: 1, currentLocation: {x: blueCircle.x - bulletSize/2, y: height - circleRadius }});
+    }   else if (colorType === 'red') {
+      redFireBullets.push({value: 1, currentLocation: {x: redCircle.x - bulletSize/2, y: circleRadius }});
+    }
+}
+
+function getCircleToMove(playerType) {
+    let circleRef;
+    if(playerType === 'player1') {
+        circleRef = blueCircle;
+    } else if (playerType === 'player2') {
+        circleRef = redCircle;
+    }
+    return circleRef;
+}
+
+/*
+function keyTyped() {
+  if(!isGameStarted()) {
+    return;
+  }
+  
+  if(playerType === 'player1')
+  moveCharacter(blueCircle, true);
+    //redCircle.x -= bulletSize; //circleRadius/2;
+  } else if (key === 'd') {
+    moveCharacter(redCircle, false);
+    //redCircle.x += bulletSize; //circleRadius/2;
+  }
+ 
+}
+*/
+
+function moveCharacter(characterRef, isLeft) {
+  if(isLeft) {
+    characterRef.x -= bulletSize - 1;
+  } else if(!isLeft) {
+    characterRef.x += bulletSize - 1;
+
+  }
+}
+
+function checkforIntersection() {
+    for(let i = 0; i < redFireBullets.length; i++) {
+        for(let j = 0; j < blueFireBullets.length; j++) {
+          console.log(redFireBullets[i]?.currentLocation?.x + '  '+redFireBullets[i]?.currentLocation?.y + ' blue--  ' +blueFireBullets[j]?.currentLocation?.x + '  '+blueFireBullets[j].currentLocation?.y);
+             if(checkBulletXCordinateForCollision(blueFireBullets[j],
+                                  redFireBullets[i]               
+                                                 ) && 
+                (redFireBullets[i]?.currentLocation?.y + bulletSize/2 >= (blueFireBullets[j]?.currentLocation?.y - bulletSize/2)
+               )) {
+                 console.log(redFireBullets.length + '  '+blueFireBullets.length);
+                  redFireBullets.splice(i, 1);
+                  blueFireBullets.splice(i, 1);
+                 console.log(redFireBullets.length + '  '+blueFireBullets.length);
+             }
+        }
+    }
+}
+
+function checkBulletXCordinateForCollision(blueBullet, redBullet) {
+  let blueX , redX;
+  if(!blueBullet || !blueBullet.currentLocation || !blueBullet.currentLocation.hasOwnProperty('x')
+ ||   !redBullet || !redBullet.currentLocation || !redBullet.currentLocation.hasOwnProperty('x')
+    ) {
+    return false;
+  }
+  blueX = blueBullet.currentLocation.x;
+  redX = redBullet.currentLocation.x;
+      if( (blueX === redX) 
+         || (blueX < redX && blueX + bulletSize > redX)
+         || (blueX > redX && blueX < redX + bulletSize)
+        ) {
+        return true;
+      }
+  return false;
+}
+
+function checkForPlayerHit() {
+  if(!redFireBullets || redFireBullets.length === 0) {
+    return;
+  }
+ // console.log('in checkforplayer hit');
+ // console.log(redFireBullets[0].currentLocation.x + '  '+redFireBullets[0].currentLocation.y +' '+blueCircle.x + ' '+blueCircle.y);
+  for(let i = 0; i < redFireBullets.length; i++) {
+    if((redFireBullets[i]?.currentLocation?.x >=  blueCircle.x - circleRadius/2 && (redFireBullets[i]?.currentLocation?.x <=  blueCircle.x + circleRadius/2) ) && 
+                (redFireBullets[i]?.currentLocation?.y >= blueCircle.y)) {
+      blueCircle = null;
+    }
+  }
+}
